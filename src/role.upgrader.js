8d1440718ea92e8700b6b15c3c8974630e098450
Jakub <jakubmuzyk@yahoo.com>
2018-12-21 14:42:34 +0100

    move role to main dir

---
+++ src/role.upgrader.js
@@ -0,0 +1,27 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.fullcheck && creep.carry.energy == 0) {
+            creep.memory.fullcheck = false;
+        }
+        if(!creep.memory.fullcheck && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.fullcheck = true;
+        }
+
+        if(creep.memory.fullcheck) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;

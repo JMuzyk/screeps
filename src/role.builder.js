4f8774aa70f63712f844df6c031c4e9673ee8252
Jakub <jakubmuzyk@yahoo.com>
2018-12-23 10:14:00 +0100

    builder can now repair

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,36 +1,37 @@
 var roleBuilder = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.memory.building && creep.carry.energy === 0) {
+    run: function (creep) {
+        if (creep.memory.building && creep.carry.energy === 0) {
             creep.memory.building = false;
         }
-        if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
+        if (!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
             creep.memory.building = true;
         }
 
-        if(creep.memory.building) {
-            if(Game.constructionSites.length > 0) {
+        if (creep.memory.building) {
+            if (Game.constructionSites.length > 0) {
                 const unfinishedBuilding = Game.constructionSites[0];
-                if(creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
+                if (creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
                     creep.say('Building');
                     creep.moveTo(unfinishedBuilding, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                const structuresToRepair = _.filter(Game.structures, (structure) => structure.hits < structure.hitsMax);
-                if(structuresToRepair.length > 0) {
+                const structuresToRepair = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => structure.hits < structure.hitsMax
+                });
+                if (structuresToRepair.length > 0) {
                     const structureToRepair = structuresToRepair[0];
-                    if(creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
+                    if (creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
                         creep.say('Repairing');
                         creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
                 }
             }
 
-        }
-        else {
+        } else {
             const sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }

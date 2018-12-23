c17c69d696cf507e9b15be7f271803f0f7e887c2
Jakub <jakubmuzyk@yahoo.com>
2018-12-23 10:00:12 +0100

    builder can now repair

--- src/role.builder.js
+++ src/role.builder.js
@@ -2,18 +2,29 @@ var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.memory.building && creep.carry.energy == 0) {
+        if(creep.memory.building && creep.carry.energy === 0) {
             creep.memory.building = false;
         }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+        if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
             creep.memory.building = true;
         }
 
         if(creep.memory.building) {
-            const unfinishedBuilding = _.filter(Game.constructionSites, (site) => site.progress < 333300)[0]
-            if(creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(unfinishedBuilding, {visualizePathStyle: {stroke: '#ffffff'}});
+            if(Game.constructionSites.length > 0) {
+                const unfinishedBuilding = Game.constructionSites[0];
+                if(creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(unfinishedBuilding, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else {
+                const structuresToRepair = _.filter(Game.structures, (structure) => structure.hits < structure.hitsMax);
+                if(structuresToRepair.length > 0) {
+                    const structureToRepair = structuresToRepair[0];
+                    if(creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
             }
+
         }
         else {
             const sources = creep.room.find(FIND_SOURCES);

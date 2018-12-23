be963c675d384586be41fb670a41a109ac46a3e8
Jakub <jakubmuzyk@yahoo.com>
2018-12-23 10:03:43 +0100

    builder can now repair

--- src/role.builder.js
+++ src/role.builder.js
@@ -13,6 +13,7 @@ var roleBuilder = {
             if(Game.constructionSites.length > 0) {
                 const unfinishedBuilding = Game.constructionSites[0];
                 if(creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
+                    creep.say('Building');
                     creep.moveTo(unfinishedBuilding, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
@@ -20,6 +21,7 @@ var roleBuilder = {
                 if(structuresToRepair.length > 0) {
                     const structureToRepair = structuresToRepair[0];
                     if(creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
+                        creep.say('Repairing');
                         creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
                 }

44ef288526c08ee601c38ff8a3e5181990bbda8d
Jakub <jakubmuzyk@yahoo.com>
2018-12-25 23:58:39 +0100

    Move number of screeps needed to constants

--- src/main.js
+++ src/main.js
@@ -4,49 +4,52 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    for(let name in Memory.creeps) {
-        if(!Game.creeps[name]) {
+    const BUILDERS_NEEDED = 1;
+    const HARVESTERS_NEEDED = 3;
+    const UPGRADERS_NEEDED = 3;
+
+    for (let name in Memory.creeps) {
+        if (!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
 
-    if(!Game.spawns['Krakow'].spawning) {
+    if (!Game.spawns['Krakow'].spawning) {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
 
-        if(harvesters.length < 3) {
+        if (harvesters.length < HARVESTERS_NEEDED) {
             const newName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
-            Game.spawns['Krakow'].spawnCreep([WORK,CARRY,MOVE], newName,
+            Game.spawns['Krakow'].spawnCreep([WORK, CARRY, MOVE], newName,
                 {memory: {role: 'harvester'}});
-        }
-        else if(upgraders.length < 3) {
+        } else if (upgraders.length < UPGRADERS_NEEDED) {
             const newName = 'Upgrader' + Game.time;
             console.log('Spawning new harvester: ' + newName);
-            Game.spawns['Krakow'].spawnCreep([WORK,CARRY,MOVE], newName,
+            Game.spawns['Krakow'].spawnCreep([WORK, CARRY, MOVE], newName,
                 {memory: {role: 'upgrader'}});
-        }
-        else if(builders.length < 2) {
+        } else if (builders.length < BUILDERS_NEEDED) {
             const newName = 'Builder' + Game.time;
             console.log('Spawning new builder: ' + newName);
-            Game.spawns['Krakow'].spawnCreep([WORK,CARRY,MOVE], newName,
+            Game.spawns['Krakow'].spawnCreep([WORK, CARRY, MOVE], newName,
                 {memory: {role: 'builder'}});
         }
+
     }
 
 
-    for(let name in Game.creeps) {
+    for (let name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role === 'harvester') {
+        if (creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role === 'upgrader') {
+        if (creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
         }
-        if(creep.memory.role === 'builder') {
+        if (creep.memory.role === 'builder') {
             roleBuilder.run(creep);
         }
     }
-}
+};

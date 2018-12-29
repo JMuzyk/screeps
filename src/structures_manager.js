197f017f389413d151687fe5a0eface4c0e4a808
Kuba <jakubmuzyk@yahoo.com>
2018-12-29 23:49:57 +0100

    debugging

--- src/structures_manager.js
+++ src/structures_manager.js
@@ -3,12 +3,13 @@ const roleTower = require('role.tower');
 const structuresManager = {
 
     run: function() {
-        // const towers =_.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_TOWER);
-        //
-        // for(let tower in towers) {
-        //     console.log(tower);
-        //     roleTower.run(tower);
-        // }
+        const towers =_.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_TOWER);
+
+        console.log(towers);
+        for(let tower in towers) {
+            console.log(tower);
+            roleTower.run(tower);
+        }
     }
 };
 

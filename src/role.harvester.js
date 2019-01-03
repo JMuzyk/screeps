5172bb00d86284b73110cabc9e84944f44c11af4
Kuba <jakubmuzyk@yahoo.com>
2019-01-03 23:58:11 +0100

    AI for harvester with many carry parts

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,31 +1,49 @@
-const roleHarvester = {
+const roleHarvester = (function () {
+
+    function deliverEnergy(creep) {
+        const spawnsAndTowers = creep.room.find(FIND_MY_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType === STRUCTURE_SPAWN ||
+                    structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            }
+        });
+
+        if (spawnsAndTowers.length > 0) {
+            if (creep.transfer(spawnsAndTowers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawnsAndTowers[0]);
+            }
+        } else {
+            const closestExtensionWithEnergyNeed = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType === STRUCTURE_EXTENSION
+                        && structure.energy < structure.energyCapacity;
+                }
+            });
+
+            if (closestExtensionWithEnergyNeed !== null) {
+                if (creep.transfer(closestExtensionWithEnergyNeed, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(closestExtensionWithEnergyNeed);
+                }
+            }
+        }
+    }
 
     /** @param {Creep} creep **/
-    run: function (creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
+    function run(creep) {
+
+        if (creep.carry.energy > 0) {
+            deliverEnergy(creep);
+        } else {
             const sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
-        } else {
-            const targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_SPAWN ||
-                        structure.structureType === STRUCTURE_EXTENSION ||
-                        structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE ) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            } else {
-                const targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => structure.structureType === STRUCTURE_SPAWN});
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            }
         }
     }
-};
+
+    return {
+        run: run
+    };
+})();
 
 module.exports = roleHarvester;

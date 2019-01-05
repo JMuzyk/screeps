360c28498d0af5854a0e491725d6ae3d3e32a23d
Jakub <jakubmuzyk@yahoo.com>
2019-01-05 10:10:51 +0100

    quick fix gathering in harvester

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -29,20 +29,20 @@ const roleHarvester = (function () {
     }
 
     function gatherEnergy(creep) {
+        const source = creep.room.find(FIND_SOURCES)[1];
         const containers = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
-                return structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0;
+                return structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0
+                    && structure.pos.findClosestByRange(FIND_SOURCES) === source;
             }
         });
-
         if (containers.length > 0) {
             if (creep.withdraw(containers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(containers[0]);
             }
         } else {
-            const sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+            if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
             }
         }
     }

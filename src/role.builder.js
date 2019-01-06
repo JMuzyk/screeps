c699b6f31f64fd56ada74a468c0b377daf4e29d6
Kuba <jakubmuzyk@yahoo.com>
2019-01-06 21:33:00 +0100

    upgraded builder

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,5 +1,7 @@
-const roleBuilder = (function(){
+const roleBuilder = (function () {
+
     function gatherEnergy(creep) {
+        //TODO get proper source from Memory
         const source = creep.room.find(FIND_SOURCES)[1];
         const containers = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
@@ -18,35 +20,70 @@ const roleBuilder = (function(){
         }
     }
 
-    function run(creep) {
-        if (creep.memory.building && creep.carry.energy === 0) {
-            creep.memory.building = false;
+    function checkIfThereIsUrgentRepairSomewhere(creep) {
+        const structuresToRepair = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits / structure.hitsMax < 0.5 && structure.hits < 250000
+        });
+        if (structuresToRepair.length > 0) {
+            delete creep.memory.constructionSiteId;
+            creep.memory.structureToRepairId = structuresToRepair[0].id;
         }
-        if (!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
-            creep.memory.building = true;
-        }
+    }
 
-        if (creep.memory.building) {
+    function repairStructure(creep, structureToRepair) {
+        if (creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+    }
+
+    function buildStructure(creep, constructionSite) {
+        if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(constructionSite, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+    }
+
+    function run(creep) {
+
+        if (creep.memory.constructionSiteId && creep.carry.energy > 0) {
+            const constructionSite = Game.getObjectById(creep.memory.constructionSiteId);
+            if (constructionSite !== null) {
+                buildStructure(creep, constructionSite);
+            }
+
+            // every 50 ticks check if there is any building with less than 50% hits, then make it repair building and remove build structure
+            if (Game.time % 50 === 0) {
+                checkIfThereIsUrgentRepairSomewhere(creep);
+            }
+
+        } else if (creep.memory.structureToRepairId && creep.carry.energy > 0) {
+            const structureToRepair = Game.getObjectById(creep.memory.structureToRepairId);
+            if (structureToRepair !== null
+                && structureToRepair.hits < structureToRepair.hitsMax && structureToRepair.hits < 300000) {
+                repairStructure(creep, structureToRepair);
+            } else {
+                delete creep.memory.structureToRepairId;
+            }
+
+        } else if (creep.carry.energy < creep.carryCapacity) {
+            delete creep.memory.constructionSiteId;
+            delete creep.memory.structureToRepairId;
+            gatherEnergy(creep);
+        } else {
             const constructionSitesIds = Object.getOwnPropertyNames(Game.constructionSites);
             if (constructionSitesIds.length > 0) {
-                const unfinishedBuilding = Game.constructionSites[constructionSitesIds[0]];
-                if (creep.build(unfinishedBuilding) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(unfinishedBuilding, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+                creep.memory.constructionSiteId = constructionSitesIds[0];
+                const constructionSite = Game.constructionSites[creep.memory.constructionSiteId];
+                buildStructure(creep, constructionSite);
             } else {
                 const structuresToRepair = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 250000
+                    filter: (structure) => structure.hits / structure.hitsMax < 0.8 && structure.hits < 250000
                 });
                 if (structuresToRepair.length > 0) {
                     const structureToRepair = structuresToRepair[0];
-                    if (creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
-                        creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
+                    creep.memory.structureToRepairId = structureToRepair.id;
+                    repairStructure(creep, structureToRepair);
                 }
             }
-
-        } else {
-            gatherEnergy(creep);
         }
     }
 

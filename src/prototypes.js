4d8bd2b0b04a6b5eca661ba80d67754f9f819f6a
Kuba <jakubmuzyk@yahoo.com>
2019-01-16 22:23:31 +0100

    prototypes testing

--- src/prototypes.js
+++ src/prototypes.js
@@ -1,6 +1,10 @@
-Creep.prototype.sayHello = function() {
-    // In prototype functions, 'this' usually has the value of the object calling
-    // the function. In this case that is whatever creep you are
-    // calling '.sayHello()' on.
-    this.say("Hello!");
-};
+const prototypes = (function(){
+    Creep.prototype.sayHello = function() {
+        // In prototype functions, 'this' usually has the value of the object calling
+        // the function. In this case that is whatever creep you are
+        // calling '.sayHello()' on.
+        this.say("Hello!");
+    };
+})();
+
+

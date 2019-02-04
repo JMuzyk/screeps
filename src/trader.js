d3413f8fed1dc0fc022e0d9f0e2962b01ab4f02a
Kuba <jakubmuzyk@yahoo.com>
2019-02-04 23:20:11 +0100

    trader

--- src/trader.js
+++ src/trader.js
@@ -5,7 +5,8 @@ const trader = (function () {
             const utriumBuyOrders = Game.market.getAllOrders(
                 (order) => {
                     return order.type === ORDER_BUY && order.resourceType === RESOURCE_UTRIUM
-                        && Game.market.calcTransactionCost(1000, room, order.roomName) < 500;
+                        && Game.market.calcTransactionCost(1000, room, order.roomName) < 1000
+                        && order.price > 0.1;
                     });
 
             console.log(JSON.stringify(utriumBuyOrders));

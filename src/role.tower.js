72ab0767c8b71618c8cb14a8b3489b53838d6212
Kuba <jakubmuzyk@yahoo.com>
2019-02-06 22:45:29 +0100

    tower healing creeps

--- src/role.tower.js
+++ src/role.tower.js
@@ -4,9 +4,14 @@ const roleTower = (function () {
         const enemyCreepsToAttack = tower.room.find(FIND_HOSTILE_CREEPS,
             {filter: (enemyCreep) => enemyCreep.pos.inRangeTo(tower.pos, 20)});
 
+        const friendlyCreepsToHeal = tower.room.find(FIND_MY_CREEPS,
+            {filter: (friendlyCreep) => friendlyCreep.hits < friendlyCreep.hitsMax});
         if (enemyCreepsToAttack.length > 0) {
             const creepToAttack = enemyCreepsToAttack[0];
             tower.attack(creepToAttack);
+        } else if (friendlyCreepsToHeal.length > 0) {
+            const creepToHeal = friendlyCreepsToHeal[0];
+            tower.heal(creepToHeal);
         }
     }
 

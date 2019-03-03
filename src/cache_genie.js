93ca20675b5fce2a9ceb4a87f20f10ab780b6128
Kuba <jakubmuzyk@yahoo.com>
2019-03-03 21:13:21 +0100

    fix when source is not found by path

--- src/cache_genie.js
+++ src/cache_genie.js
@@ -5,10 +5,10 @@ const cacheGenie = (function(){
             Memory[room.name] = {};
         }
         if(typeof Memory[room.name].resourceClosestToController === 'undefined') {
-            const source = room.controller.pos.findClosestByPath(FIND_SOURCES);
-            console.log(room.controller.pos);
-            console.log(source);
-            console.log(room.controller.pos.findClosestByRange(FIND_SOURCES));
+            let source = room.controller.pos.findClosestByPath(FIND_SOURCES);
+            if(source === null) {
+                source = room.controller.pos.findClosestByRange(FIND_SOURCES);
+            }
             Memory[room.name].resourceClosestToController = source.id;
         }
         return Memory[room.name].resourceClosestToController;

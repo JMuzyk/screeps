2db37db15db8c61bd4699f80dcc0d76e1a2b5adb
Kuba <jakubmuzyk@yahoo.com>
2019-03-03 21:11:10 +0100

    debug

--- src/cache_genie.js
+++ src/cache_genie.js
@@ -8,7 +8,7 @@ const cacheGenie = (function(){
             const source = room.controller.pos.findClosestByPath(FIND_SOURCES);
             console.log(room.controller.pos);
             console.log(source);
-            console.log(room.controller.pos.findClosestByPath(FIND_SOURCES));
+            console.log(room.controller.pos.findClosestByRange(FIND_SOURCES));
             Memory[room.name].resourceClosestToController = source.id;
         }
         return Memory[room.name].resourceClosestToController;

723a237446a0d1c3a709ae611d2a1b33d140c6eb
Kuba <jakubmuzyk@yahoo.com>
2019-03-03 21:54:04 +0100

    debug

--- src/creeps_factory.js
+++ src/creeps_factory.js
@@ -6,6 +6,8 @@ const creepsFactory = (function () {
         const idleSpawns = room.find(FIND_MY_SPAWNS, {
             filter: (spawn) => !spawn.spawning
         });
+        console.log(idleSpawns);
+        console.log(room.find(FIND_MY_SPAWNS));
         return idleSpawns > 0;
     }
 

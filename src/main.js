08a02068637cf9879c888224f1208fecaec86f95
Jakub <jakubmuzyk@yahoo.com>
2019-03-04 00:15:05 +0100

    add profiler

--- src/main.js
+++ src/main.js
@@ -2,20 +2,26 @@ const creepsManager = require('creeps_manager');
 const structuresManager = require('structures_manager');
 const prototypes = require('prototypes');
 const trader = require('trader');
+const profiler = require('screeps-profiler');
+
+profiler.enable();
 
 module.exports.loop = function () {
 
-   for (let name in Memory.creeps) {
-      if (!Game.creeps[name]) {
-         delete Memory.creeps[name];
-         console.log('Clearing non-existing creep memory:', name);
+   profiler.wrap(function(){
+      for (let name in Memory.creeps) {
+         if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+         }
       }
-   }
 
-   for (let roomName in Game.rooms) {
-      creepsManager.run(Game.rooms[roomName]);
-   }
+      for (let roomName in Game.rooms) {
+         creepsManager.run(Game.rooms[roomName]);
+      }
 
-   structuresManager.run();
-   trader.run();
+      structuresManager.run();
+      trader.run();
+   });
+
 };
